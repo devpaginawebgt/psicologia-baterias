cc37fa94692a98c15cb752ab4085f7d8245ca4bc
Dennis Herrera <dennishumher25@hotmail.com>
2025-05-26 16:57:59 -0600

    feat: created basic routes and moved files to modular folders

--- routes/auth.php
+++ routes/auth.php
@@ -4,32 +4,39 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::middleware('guest')->group(function () {
-    Volt::route('login', 'auth.login')
-        ->name('login');
-
-    Volt::route('register', 'auth.register')
-        ->name('register');
-
-    Volt::route('forgot-password', 'auth.forgot-password')
-        ->name('password.request');
-
-    Volt::route('reset-password/{token}', 'auth.reset-password')
-        ->name('password.reset');
-
+Route::prefix('auth')->as('auth')->group(function () {
+    Route::get('login', function() {
+        return view('auth/pages/login');
+    })->name('.login');
 });
 
-Route::middleware('auth')->group(function () {
-    Volt::route('verify-email', 'auth.verify-email')
-        ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
+// Route::middleware('guest')->group(function () {
+//     Volt::route('login', 'auth.login')
+//         ->name('login');
 
-    Volt::route('confirm-password', 'auth.confirm-password')
-        ->name('password.confirm');
-});
+//     Volt::route('register', 'auth.register')
+//         ->name('register');
 
-Route::post('logout', App\Livewire\Actions\Logout::class)
-    ->name('logout');
+//     Volt::route('forgot-password', 'auth.forgot-password')
+//         ->name('password.request');
+
+//     Volt::route('reset-password/{token}', 'auth.reset-password')
+//         ->name('password.reset');
+
+// });
+
+// Route::middleware('auth')->group(function () {
+//     Volt::route('verify-email', 'auth.verify-email')
+//         ->name('verification.notice');
+
+//     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+//         ->middleware(['signed', 'throttle:6,1'])
+//         ->name('verification.verify');
+
+//     Volt::route('confirm-password', 'auth.confirm-password')
+//         ->name('password.confirm');
+// });
+
+// Route::post('logout', App\Livewire\Actions\Logout::class)
+//     ->name('logout');
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,30 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// Index
+Route::get('/', function() {
+    return redirect()->route('auth.login');
 });
 
+// Auth routes
 require __DIR__.'/auth.php';
+
+// Signed in routes
+Route::group([], function() {
+    require __DIR__.'/batteries.php';
+}); 
+
+
+
+//? Default routes
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
+
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
