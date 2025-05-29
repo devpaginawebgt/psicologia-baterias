d6eec73d4f3c6b3001eea5fe5c421909f890a8b7
Dennis Herrera <dennishumher25@hotmail.com>
2025-05-29 16:30:08 -0600

    chore: fixed navbar style to center logos and logout

--- resources/views/components/layouts/batteries-layout.blade.php
+++ resources/views/components/layouts/batteries-layout.blade.php
@@ -25,7 +25,7 @@
     </head>
 
     <body class="flex flex-col">
-        <header class="w-full bg-zinc-900 p-3 flex justify-between items-center border-b border-zinc-700">
+        <header class="w-full bg-zinc-900 p-3 grid grid-cols-2 lg:grid-cols-3 items-center border-b border-zinc-700">
             <div class="flex items-center gap-3 cursor-default">
                 <img
                     src="{{ $company->logo }}"
@@ -35,7 +35,7 @@
                 <span class="text-sm lg:text-base">{{ $company->name }}</span>
             </div>
 
-            <div class="hidden lg:flex">
+            <div class="hidden lg:flex lg:justify-center">
                 <img
                     src="/logos/Logo Loasa.png"
                     alt=""
@@ -47,6 +47,7 @@
                 action="{{ route('batteries.logout') }}"
                 method="POST"
                 id="logoutForm"
+                class="flex justify-end"
             >
                 @csrf
                 <button
@@ -60,13 +61,13 @@
         </header>
 
         <div class="flex h-full grow">
-            <x-mary-menu class="p-4 bg-zinc-900 w-max border-r border-zinc-700">
+            <x-mary-menu class="p-4 bg-zinc-900 min-w-40 w-max border-r border-zinc-700 hidden lg:flex">
                 @foreach($batteries as $battery)
                     <x-mary-menu-item class="-ml-2" href="{{ $battery->url }}">
                         <div class="flex items-center gap-2">
                             <x-mary-icon
                                 name="o-clipboard"
-                                class="w-7"
+                                class="w-5"
                             />
                             {{ $battery->name }}
                         </div>
