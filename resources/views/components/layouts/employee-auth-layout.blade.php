c377b0ecc9c1729b82c6fcba5dc09ced28356168
Dennis Herrera <dennishumher25@hotmail.com>
2025-07-09 10:33:10 -0600

    chore: changed page icon

--- resources/views/components/layouts/employee-auth-layout.blade.php
+++ resources/views/components/layouts/employee-auth-layout.blade.php
@@ -11,7 +11,6 @@
         <title>{{ $title }} - {{ config('app.name'); }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <!-- Fonts -->
