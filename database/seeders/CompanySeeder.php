adf6a98d7d8380b4c604adcd5b8b8dd007d1bf04
Dennis Herrera <dennishumher25@hotmail.com>
2025-07-09 11:35:25 -0600

    chore: renamed company in seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -15,7 +15,7 @@ class CompanySeeder extends Seeder
     {
         Company::create([
             'country_id' => 1,
-            'name' => 'Farmacia Ascavi',
+            'name' => 'Farmacias Ascavi, Carex, Selectpharma y El Punto',
             'logo' => '/logos/Logo Farmacia Ascavi.png',
             'is_active' => true,
         ]);
