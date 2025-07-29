bf30e3f869bf5ab516b7eb919044008d8dfb0f2c
Dennis Herrera <dennishumher25@hotmail.com>
2025-07-29 15:20:29 -0600

    fix: dashboard layout and iframe styles

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,4 @@
-<main class="w-full p-4 lg:p-8">
+<main class="w-full p-4 lg:px-8 lg:py-5">
     <div class="w-full flex flex-col items-center">
         <h1 class="text-center text-xl uppercase font-semibold mb-4">
             Dashboard Psicolasa
@@ -9,7 +9,7 @@
             src="https://app.powerbi.com/view?r=eyJrIjoiNjRhZDYzMTItYzg3Ni00MzUyLWEzYWUtMzY5M2EzNDVjYTQxIiwidCI6ImM4YzRhNTQxLWM2YzAtNDI2YS1hNzhiLWZlZDIwYmY3MDRlZiIsImMiOjR9"
             frameborder="0"
             allowFullScreen="true"
-            class="aspect-[16/10] max-w-[72rem] rounded-md"
+            class="aspect-[16/10] max-w-[80rem] rounded-md"
         ></iframe>
     </div>
 </main>
