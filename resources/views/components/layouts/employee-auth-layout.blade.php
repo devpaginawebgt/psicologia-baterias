9f1f674e52a7e16d1e8f737cf823be8cb6c38d62
Dennis Herrera <dennishumher25@hotmail.com>
2026-01-30 16:00:24 -0600

    fix: csrf token validation through head meta

--- resources/views/components/layouts/employee-auth-layout.blade.php
+++ resources/views/components/layouts/employee-auth-layout.blade.php
@@ -18,6 +18,9 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         @vite(['resources/css/app.css', 'resources/css/main.css', 'resources/js/app.js'])
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
     </head>
 
     <body class="flex justify-center items-center py-8 px-4">
